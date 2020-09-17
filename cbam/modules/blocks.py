c418a8f6f48134217f50cc55a9ac7fdcc2524804
Toraudonn <toraudonn@gmail.com>
2020-09-17 14:48:05 +0000

    add bam

--- cbam/modules/blocks.py
+++ cbam/modules/blocks.py
@@ -85,8 +85,10 @@ class Bottleneck(nn.Module):
             planes, planes, kernel_size=3, stride=stride, padding=1, bias=False
         )
         self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(planes * 4)
+        self.conv3 = nn.Conv2d(
+            planes, planes * self.expansion, kernel_size=1, bias=False
+        )
+        self.bn3 = nn.BatchNorm2d(planes * self.expansion)
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
         self.stride = stride
